bf12fbd0547c0d2d502abfacce80b566222495ab
JeffreyMeijer <123974@student.drenthecollege.nl>
2019-03-15 09:00:26 +0100

    Extra methodes toegevoegd

--- Charactersweg.java
+++ Charactersweg.java
@@ -2,7 +2,9 @@ package opdrachten;
 
 public class Charactersweg {
 	public static void main(String[] args) {
-		System.out.println(RemoveChar(args[0]));
+		System.out.println("Methode 1: " + RemoveChar(args[0]));
+		System.out.println("Methode 2: " + Methode2(args[0]));
+		System.out.println("Methode 3: " + Methode3(args[0]));
 	}
 	
 	//Methode 1
@@ -12,5 +14,13 @@ public class Charactersweg {
 			string = string.replace(weg[i], "");
 		}
 		return string;
+	}
+	//Methode 2
+	public static String Methode2(String string) {
+		return string.replaceAll("[a-e]","");
+	}
+	//Methode 3
+	public static String Methode3(String string) {
+		return string.replaceFirst("h", "b");
 	}
 }
